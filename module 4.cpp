a31876384b140268c6865fc87a9893568464bb09
RoMe025 <sergei.gavrushenko830@mail.ru>
2025-11-27 23:12:40 +0300

    Add files via upload

---
+++ module 4.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    using namespace std;
+    int number;
+    cout << "Введите целое число: ";
+    cin >> number;
+    int result = (number % 2 == 0 && number > 0) ? (number << 2) : (number >> 2);
+    cout <<  number << std::endl;
+    cout  << result << std::endl;
+    return 0;
+}
